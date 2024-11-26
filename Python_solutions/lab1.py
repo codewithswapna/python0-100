2d2a4b2bf5a4bed23dc7974d14de8802f09424ca
codewithswapna <163385475+codewithswapna@users.noreply.github.com>
2024-11-26 16:46:02 +0000

    Task1

---
+++ Python_solutions/lab1.py
@@ -0,0 +1,24 @@
+'''1- Calculate the multiplication and sum of two numbers'''
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+multiplication = num1 * num2
+summation = num1 + num2
+
+print(f"Multiplication: {multiplication}")
+print(f"Sum: {summation}")
+
+'''2-Declare two variables and print that which variable is largest using ternary operators'''
+a = 10
+b = 20
+largest = a if a > b else b
+
+print(f"The largest variable is: {largest}")
+
+
+'''3-Python program to convert the temperature in degree centigrade to Fahrenheit'''
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit.")
+
+
+'''4-'''
